275acbcb8da845fc0bc1f81a6e7bab290c164b92
Woojung <woo_jung2@naver.com>
2020-02-05 16:11:37 +0900

    Update 백준_회의실배정.cpp

--- 0205_Greedy/백준_회의실배정.cpp
+++ 0205_Greedy/백준_회의실배정.cpp
@@ -5,26 +5,35 @@
 #include <vector>
 using namespace std;
 
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
 int main() {
 
 	int n,x,y,a,cnt;
+	int cBegin[100000], cEnd[100000];
 	vector<pair<int, int>> time;
+	
 	cin >> n;
 
-	for (int i = 0;i < n;i++)
-		cin >> x >> y;
-		time.push_back(pair<int, int>(x, y));
+	for (int i = 0; i < n; i++)
+		cin >> cBegin[i] >> cEnd[i];
+	for (int i = 0; i < n; i++)
+		time.push_back(make_pair(cEnd[i], cBegin[i]));
 
 	sort(time.begin(), time.end());
 
-	a = 0;
+	a = time[0].first;
 	cnt = 1;
 
 	for (int i = 0;i < n;i++)
-		if (time[i].first > time[a].second)
-			a = time[i].second;
-			cnt += 1;
-
+		if (time[i].second > a)
+		{
+			a = time[i].first;
+			cnt ++;
+		}
 	cout << cnt << endl;
 
 	return 0;
