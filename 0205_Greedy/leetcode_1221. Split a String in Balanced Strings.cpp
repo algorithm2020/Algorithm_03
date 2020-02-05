2b015aa87d0ba0b1aaf18018e1756f02b7dbf274
Woojung <woo_jung2@naver.com>
2020-02-05 17:35:27 +0900

    Create leetcode_1221. Split a String in Balanced Strings.cpp

---
+++ 0205_Greedy/leetcode_1221. Split a String in Balanced Strings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int cnt = 0;
+        int res = 0;
+        
+        for (int i=0; i<s.size();i++)
+        {
+            if (s[i] == 'R')
+            {
+                cnt++;
+            }
+            else {
+                cnt--;
+            }
+            if (cnt == 0)
+            {
+                res++;
+            }
+        }
+        
+        return res;
+    }
+};
