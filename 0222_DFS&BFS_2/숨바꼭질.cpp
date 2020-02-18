1b9ea01f150aee3d5dcaeb219f8d373798a3ef9a
Woojung <woo_jung2@naver.com>
2020-02-18 18:48:59 +0900

    Update 숨바꼭질.cpp

--- 0222_DFS&BFS_2/숨바꼭질.cpp
+++ 0222_DFS&BFS_2/숨바꼭질.cpp
@@ -9,8 +9,8 @@ using namespace std;
 int N, K;
 int cnt = 0;
 queue<int> q;
-bool visit[100000];
-int dist[100000];
+bool visit[100001];//[100000]로 하니까 런타임에러
+int dist[100001]; //[100000]로 하니까 런타임에러
 
 void bfs(int x )
 {
@@ -27,19 +27,19 @@ void bfs(int x )
 			break;
 		}
 	
-		if (visit[a - 1] == false && a - 1 >= 0) { //x-1 이동한 경우
+		if (visit[a - 1] == false && a - 1 >= 0 && a-1 <=100000) { //x-1 이동한 경우
 			visit[a - 1] = true;
 			dist[a - 1] = dist[a] + 1;
 			q.push(a - 1);
 		}
 
-		if (visit[a + 1] == false && a + 1 >= 0) { //x+1 이동한 경우
+		if (visit[a + 1] == false && a + 1 >= 0 && a+1 <= 100000) { //x+1 이동한 경우
 			visit[a + 1] = true;
 			dist[a + 1] = dist[a] + 1;
 			q.push(a + 1);
 		}
 
-		if (visit[2*a] == false && 2*a >= 0) { //2*x 이동한 경우
+		if (visit[2*a] == false && 2*a >= 0 && 2*a <= 100000) { //2*x 이동한 경우
 			visit[2*a] = true;
 			dist[2*a] = dist[a] + 1;
 			q.push(2*a);
