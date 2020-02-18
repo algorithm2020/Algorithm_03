d2a3a227e1e88f085c3c6c6f68d10c0fd10888aa
Woojung <woo_jung2@naver.com>
2020-02-18 18:47:40 +0900

    Create 숨바꼭질.cpp

---
+++ 0222_DFS&BFS_2/숨바꼭질.cpp
@@ -0,0 +1,59 @@
+// BFS 풀이
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+
+int N, K;
+int cnt = 0;
+queue<int> q;
+bool visit[100000];
+int dist[100000];
+
+void bfs(int x )
+{
+	q.push(x);
+	visit[x] = true;
+	dist[x] = 0;
+	
+	while (!q.empty())
+	{
+		int a = q.front();
+
+		if (a == K) {
+			cout << dist[a] << '\n';
+			break;
+		}
+	
+		if (visit[a - 1] == false && a - 1 >= 0) { //x-1 이동한 경우
+			visit[a - 1] = true;
+			dist[a - 1] = dist[a] + 1;
+			q.push(a - 1);
+		}
+
+		if (visit[a + 1] == false && a + 1 >= 0) { //x+1 이동한 경우
+			visit[a + 1] = true;
+			dist[a + 1] = dist[a] + 1;
+			q.push(a + 1);
+		}
+
+		if (visit[2*a] == false && 2*a >= 0) { //2*x 이동한 경우
+			visit[2*a] = true;
+			dist[2*a] = dist[a] + 1;
+			q.push(2*a);
+		}
+		q.pop();
+		
+	}
+}
+
+int main()
+{
+	cin >> N >> K;
+
+	bfs(N);
+
+	return 0;
+}
