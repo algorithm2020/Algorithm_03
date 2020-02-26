370d1a058cb3af7e5ddd31f70f519aed9114286d
Woojung <woo_jung2@naver.com>
2020-02-26 20:16:32 +0900

    Create K번째수_1.cpp

---
+++ 프로그래머스_코테연습/정렬/K번째수_1.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    vector<int> slice;
+    int cnt = 0;
+    for (int i=0; i<commands.size();i++)
+    {
+        for (int j=commands[i][0]-1; j<commands[i][1];j++)
+        {
+            slice.push_back(array[j]);
+            cnt++;
+        }
+        sort(slice.begin(),slice.end());
+        answer.push_back(slice[commands[i][2]-1]);
+        
+        for (int k=0;k<cnt;k++) 
+        {   
+            slice.pop_back(); 
+        }
+        cnt = 0;
+    }
+    return answer;
+}
