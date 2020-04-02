604f5f6462af80b3bd41d858e8d5bac65cbd68f1
Woojung <woo_jung2@naver.com>
2020-04-02 19:11:49 +0900

    Update 단어변환.cpp

--- 0404/단어변환.cpp
+++ 0404/단어변환.cpp
@@ -5,9 +5,9 @@
 using namespace std;
 
 int solution(string begin, string target, vector<string> words) {
-    int answer = 0;
-    bool visit[50] = {false,};
-    bool check = false;
+    	int answer = 0;
+    	bool visit[50] = {false,};
+    	bool check = false;
 	int cnt;
 	queue<pair<string,int>> q;
     
@@ -19,7 +19,7 @@ int solution(string begin, string target, vector<string> words) {
 	} 
 	if (check == false) return 0;
 
-    q.push(make_pair(begin, 0));
+   	q.push(make_pair(begin, 0));
 
 	while (!q.empty()) {
 		string cmp = q.front().first;
@@ -28,20 +28,20 @@ int solution(string begin, string target, vector<string> words) {
 
 		if (cmp == target) {
 			answer = dist;
-            break;
+            		break;
 		}
 		for (int i = 0; i < words.size(); i++) {
-            cnt = 0;
+            		cnt = 0;
 			if ( visit[i] == false) {
 				for (int j = 0;j < cmp.size();j++) {
 					if ( cmp[j] != words[i][j]) cnt++;
-					}
-                if (cnt == 1) {
-						q.push(make_pair(words[i],dist+1));
-						visit[i] = true;
+				}
+               			if (cnt == 1) {
+					q.push(make_pair(words[i],dist+1));
+					visit[i] = true;
 				}
 			}
 		} 
-    }
+    	}
     return answer;
 }
